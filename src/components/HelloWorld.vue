8fada4f3690a8a55519a4543979b7cb6b1cc633e
zzq <531226667@qq.com>
2022-03-15 17:24:51 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      hello -test-add
+      hello -test-add0-d
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
